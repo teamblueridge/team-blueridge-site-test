00d9a5432c142a203ed33c60bd208e2cc640d584
Robert Sink <insink71@gmail.com>
2012-07-22 23:17:14 -0300

    your asides-news is html.. fixing my mistake

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
   </article>
   <aside>
     <h3>News</h3>
-	<?php include('includes/aside-news.ssi'); ?>
+	<?php include('includes/aside-news.html'); ?>
   </aside>
   <footer>
     <p id="footer-nav"><a href="/">Home</a> | <a href="donations.html">Donate</a> | <a href="the-team.html">The Team</a> | <a href="/blog/">Blog</a> | <a href="/forum/">Forum</a> | <a href="/wiki/">Wiki</a></p>
